d752174498a825bcae1a3b03ff8cdb0bdb1abd21
RSMarian <marianstefan1499@gmail.com>
2022-06-18 08:37:35 +0300

    added behavior if input is correct

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ form.addEventListener("submit", function (e) {
   //empty value
   //-1
   //> 9(paragraphs)
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText.map(function(item){
+      return `<p class="result">${item}</p>`
+    })
+    .join("");
+    result.innerHTML = tempText;
   }
 });
